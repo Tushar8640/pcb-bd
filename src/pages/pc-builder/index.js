925ce33de009411cf887c447fbbc37e1d536d2ca
Tushar@8640 <imtantushar44@gmail.com>
2023-07-29 03:05:26 +0600

    pc build item completed

--- src/pages/pc-builder/index.js
+++ src/pages/pc-builder/index.js
@@ -1,3 +1,4 @@
+import PcBuilderItem from "@/components/PcBuilderItem";
 import { Button } from "@/components/ui/button";
 import { PlusSquare } from "lucide-react";
 import Link from "next/link";
@@ -5,143 +6,23 @@ import Link from "next/link";
 /* eslint-disable @next/next/no-img-element */
 export default function PcBuilder() {
   return (
-    <div className="container mx-auto flex justify-center ">
-      <div className="flex flex-col max-w-3xl p-6 space-y-4 sm:p-10 bg-secondary text-primary ">
+    <div className="container mx-auto flex justify-center w-full">
+      <div className="flex flex-col w-[700px] p-6 space-y-4 sm:p-10 bg-secondary text-primary ">
         <h2 className="text-xl font-semibold">Your Build</h2>
         <ul className="flex flex-col divide-y divide-gray-300">
-          <li className="flex flex-col py-6 sm:flex-row sm:justify-between">
-            <div className="flex w-full space-x-2 sm:space-x-4">
-              <img
-                className="flex-shrink-0 object-cover w-20 h-20 border-transparent rounded outline-none sm:w-32 sm:h-32 bg-gray-500"
-                src="https://images.unsplash.com/photo-1526170375885-4d8ecf77b99f?ixlib=rb-1.2.1&amp;ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;auto=format&amp;fit=crop&amp;w=1350&amp;q=80"
-                alt="Polaroid camera"
-              />
-              <div className="flex flex-col justify-between w-full pb-4">
-                <div className="flex justify-between w-full pb-2 space-x-2">
-                  <div className="space-y-1">
-                    <h3 className="text-lg font-semibold leadi sm:pr-8">
-                      Processor
-                    </h3>
-                    <p className="text-sm text-secondary-foreground">Classic</p>
-                  </div>
-                  <div className="text-right">
-                    <p className="text-lg font-semibold">59.99€</p>
-                    <p className="text-sm line-through text-gray-400">75.50€</p>
-                  </div>
-                </div>
-                <div className="flex text-sm divide-x">
-                  <Link href={"/pc-builder/category/cpu"}>
-                    <Button varient="secondary">
-                      <PlusSquare />
-                    </Button>
-                  </Link>
-
-                  <button
-                    type="button"
-                    className="flex items-center px-2 py-1 space-x-1"
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 512 512"
-                      className="w-4 h-4 fill-current"
-                    >
-                      <path d="M453.122,79.012a128,128,0,0,0-181.087.068l-15.511,15.7L241.142,79.114l-.1-.1a128,128,0,0,0-181.02,0l-6.91,6.91a128,128,0,0,0,0,181.019L235.485,449.314l20.595,21.578.491-.492.533.533L276.4,450.574,460.032,266.94a128.147,128.147,0,0,0,0-181.019ZM437.4,244.313,256.571,425.146,75.738,244.313a96,96,0,0,1,0-135.764l6.911-6.91a96,96,0,0,1,135.713-.051l38.093,38.787,38.274-38.736a96,96,0,0,1,135.765,0l6.91,6.909A96.11,96.11,0,0,1,437.4,244.313Z"></path>
-                    </svg>
-                    <span>Add to favorites</span>
-                  </button>
-                </div>
-              </div>
-            </div>
-          </li>
-          <li className="flex flex-col py-6 sm:flex-row sm:justify-between">
-            <div className="flex w-full space-x-2 sm:space-x-4">
-              <img
-                className="flex-shrink-0 object-cover w-20 h-20 border-transparent rounded outline-none sm:w-32 sm:h-32 bg-gray-500"
-                src="https://images.unsplash.com/photo-1526170375885-4d8ecf77b99f?ixlib=rb-1.2.1&amp;ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;auto=format&amp;fit=crop&amp;w=1350&amp;q=80"
-                alt="Polaroid camera"
-              />
-              <div className="flex flex-col justify-between w-full pb-4">
-                <div className="flex justify-between w-full pb-2 space-x-2">
-                  <div className="space-y-1">
-                    <h3 className="text-lg font-semibold leadi sm:pr-8">
-                      Motherboard
-                    </h3>
-                    <p className="text-sm text-secondary-foreground">Classic</p>
-                  </div>
-                  <div className="text-right">
-                    <p className="text-lg font-semibold">59.99€</p>
-                    <p className="text-sm line-through text-gray-400">75.50€</p>
-                  </div>
-                </div>
-                <div className="flex text-sm divide-x">
-                  <Link href={"/pc-builder/category/motherboard"}>
-                    <Button varient="secondary">
-                      <PlusSquare />
-                    </Button>
-                  </Link>
-
-                  <button
-                    type="button"
-                    className="flex items-center px-2 py-1 space-x-1"
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 512 512"
-                      className="w-4 h-4 fill-current"
-                    >
-                      <path d="M453.122,79.012a128,128,0,0,0-181.087.068l-15.511,15.7L241.142,79.114l-.1-.1a128,128,0,0,0-181.02,0l-6.91,6.91a128,128,0,0,0,0,181.019L235.485,449.314l20.595,21.578.491-.492.533.533L276.4,450.574,460.032,266.94a128.147,128.147,0,0,0,0-181.019ZM437.4,244.313,256.571,425.146,75.738,244.313a96,96,0,0,1,0-135.764l6.911-6.91a96,96,0,0,1,135.713-.051l38.093,38.787,38.274-38.736a96,96,0,0,1,135.765,0l6.91,6.909A96.11,96.11,0,0,1,437.4,244.313Z"></path>
-                    </svg>
-                    <span>Add to favorites</span>
-                  </button>
-                </div>
-              </div>
-            </div>
-          </li>
-          <li className="flex flex-col py-6 sm:flex-row sm:justify-between">
-            <div className="flex w-full space-x-2 sm:space-x-4">
-              <img
-                className="flex-shrink-0 object-cover w-20 h-20 border-transparent rounded outline-none sm:w-32 sm:h-32 bg-gray-500"
-                src="https://images.unsplash.com/photo-1526170375885-4d8ecf77b99f?ixlib=rb-1.2.1&amp;ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;auto=format&amp;fit=crop&amp;w=1350&amp;q=80"
-                alt="Polaroid camera"
-              />
-              <div className="flex flex-col justify-between w-full pb-4">
-                <div className="flex justify-between w-full pb-2 space-x-2">
-                  <div className="space-y-1">
-                    <h3 className="text-lg font-semibold leadi sm:pr-8">
-                      Ram
-                    </h3>
-                    <p className="text-sm text-secondary-foreground">Classic</p>
-                  </div>
-                  <div className="text-right">
-                    <p className="text-lg font-semibold">59.99€</p>
-                    <p className="text-sm line-through text-gray-400">75.50€</p>
-                  </div>
-                </div>
-                <div className="flex text-sm divide-x">
-                  <Link href={"/pc-builder/category/ram"}>
-                    <Button varient="secondary">
-                      <PlusSquare />
-                    </Button>
-                  </Link>
-
-                  <button
-                    type="button"
-                    className="flex items-center px-2 py-1 space-x-1"
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 512 512"
-                      className="w-4 h-4 fill-current"
-                    >
-                      <path d="M453.122,79.012a128,128,0,0,0-181.087.068l-15.511,15.7L241.142,79.114l-.1-.1a128,128,0,0,0-181.02,0l-6.91,6.91a128,128,0,0,0,0,181.019L235.485,449.314l20.595,21.578.491-.492.533.533L276.4,450.574,460.032,266.94a128.147,128.147,0,0,0,0-181.019ZM437.4,244.313,256.571,425.146,75.738,244.313a96,96,0,0,1,0-135.764l6.911-6.91a96,96,0,0,1,135.713-.051l38.093,38.787,38.274-38.736a96,96,0,0,1,135.765,0l6.91,6.909A96.11,96.11,0,0,1,437.4,244.313Z"></path>
-                    </svg>
-                    <span>Add to favorites</span>
-                  </button>
-                </div>
-              </div>
-            </div>
-          </li>
-    
+          <PcBuilderItem title="Processor" link="/pc-builder/category/cpu" />
+          <PcBuilderItem
+            title="Motherboard"
+            link="/pc-builder/category/motherboard"
+          />
+          <PcBuilderItem title="Ram" link="/pc-builder/category/ram" />
+          <PcBuilderItem title="Monitor" link="/pc-builder/category/monitor" />
+          <PcBuilderItem
+            title="Powersupply"
+            link="/pc-builder/category/powersupply"
+          />
+          <PcBuilderItem title="Storage" link="/pc-builder/category/storage" />
+          <PcBuilderItem title="Others" link="/pc-builder/category/others" />
         </ul>
         <div className="space-y-1 text-right">
           <p>
@@ -153,13 +34,10 @@ export default function PcBuilder() {
           </p>
         </div>
         <div className="flex justify-end space-x-4">
-          <Button
-        varient="primary"
-          >
+          <Button varient="primary">
             Back
             <span className="sr-only sm:not-sr-only">to shop</span>
           </Button>
-   
         </div>
       </div>
     </div>
