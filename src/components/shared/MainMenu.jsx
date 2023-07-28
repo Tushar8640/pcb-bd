cd1ccb41aa6da5dd0c2a32c122171daeae84321b
Tushar@8640 <imtantushar44@gmail.com>
2023-07-28 11:04:23 +0600

    initial setup done

---
+++ src/components/shared/MainMenu.jsx
@@ -0,0 +1,42 @@
+import Link from "next/link";
+
+import { buttonVariants } from "../ui/button";
+import { ThemeToggle } from "../theme-toggle";
+import { Github, TwitterIcon } from "lucide-react";
+import NavMenu from "./NavMenu";
+
+export default function MainMenu() {
+  return (
+    <header className="bg-background sticky top-0 z-40 w-full border-b">
+      <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
+        <h2 className="font-bold text-2xl">PCB-BD</h2>
+        <div className="flex flex-1 items-center justify-end space-x-4">
+          <nav className="flex items-center space-x-1">
+            <NavMenu />
+            <Link href={"/"}>
+              <div
+                className={buttonVariants({
+                  size: "icon",
+                  variant: "ghost",
+                })}
+              >
+                <Github />
+              </div>
+            </Link>
+            <Link href={"/"}>
+              <div
+                className={buttonVariants({
+                  size: "icon",
+                  variant: "ghost",
+                })}
+              >
+                <TwitterIcon />
+              </div>
+            </Link>
+            <ThemeToggle />
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+}
